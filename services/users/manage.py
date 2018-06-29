4649657844419cfe6dd44fde0ddc62cf4c1f62c7
Caroline Wanjiku <caroline.wanjiku@andela.com>
2018-06-29 14:48:47 +0300

    ft(travis)Integrate travis - Add flake 8 - Enable coverage for tests

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,9 +1,19 @@
 import unittest
+import coverage
 from flask.cli import FlaskGroup
 
 from project import create_app, db
 from project.api.models import User
 
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py'
+    ]
+)
+COV.start()
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
@@ -31,6 +41,22 @@ def seed_db():
     db.session.add(User(username='mitch', email='mitch@email.com'))
     db.session.add(User(username='johnson', email='johnson@email.com'))
     db.session.commit()
+
+
+@cli.command()
+def cov():
+    """Runs the unittests with coverage"""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary: ')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
 
 
 if __name__ == "__main__":
